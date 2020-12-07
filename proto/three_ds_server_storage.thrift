c6d0565021d632c6ffb857b67c12be361d01457e
Anatolii Karlov <a.karlov@rbkmoney.com>
2020-12-07 11:39:39 +0300

    BJ-1018: refactor namespace (#13)
    refactor pom

--- proto/three_ds_server_storage.thrift
+++ proto/three_ds_server_storage.thrift
@@ -1,4 +1,4 @@
-namespace java com.rbkmoney.damsel.three_ds_server_storage
+namespace java com.rbkmoney.damsel.threeds.server.storage
 namespace erlang three_ds_server_storage
 
 typedef string ThreeDsServerTransactionID
